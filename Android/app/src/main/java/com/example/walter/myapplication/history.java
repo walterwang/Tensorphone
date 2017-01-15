c30c90eca9715f2e2fa3265ff51c51ce696c5ac0
WalterWangNYUMLC <mr.walterwang@gmail.com>
2017-01-15 17:23:09 -0500

    updated android folder

---
+++ Android/app/src/main/java/com/example/walter/myapplication/history.java
@@ -0,0 +1,35 @@
+package com.example.walter.myapplication;
+
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+import java.io.File;
+
+public class history extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        File mPath = new File(Environment.getExternalStorageDirectory()+"//DIR//");
+        FileDialog fileDialog = new FileDialog(this, mPath, ".txt");
+        fileDialog.addFileListener(new FileDialog.FileSelectedListener() {
+            public void fileSelected(File file) {
+                Log.d(getClass().getName(), "selected file " + file.toString());
+            }
+        });
+        //fileDialog.addDirectoryListener(new FileDialog.DirectorySelectedListener() {
+        //  public void directorySelected(File directory) {
+        //      Log.d(getClass().getName(), "selected dir " + directory.toString());
+        //  }
+        //});
+        //fileDialog.setSelectDirectoryOption(false);
+        fileDialog.showDialog();
+    }
+
+}
